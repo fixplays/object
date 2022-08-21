5c19f72b5c13fc1209e9c4f24b96f1c9f5a5b5ba
fixplays <dmitriyomeltschenko@gmail.com>
2022-08-21 18:01:30 +0300

    Page transitions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage):
 
@@ -9,5 +9,7 @@ class MainPage(BasePage):
         login_link.click()
 
 
+
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
