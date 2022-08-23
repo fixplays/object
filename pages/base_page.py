b58d1fffd6ea9f4534ccca9117c8d7502c2e6b2b
fixplays <dmitriyomeltschenko@gmail.com>
2022-08-23 19:50:14 +0300

    My Finish

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,22 +1,84 @@
-from selenium.common.exceptions import NoSuchElementException
-class BasePage():
+import math
+from typing import Sequence
+
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import Remote
+from selenium.webdriver.support import expected_conditions as ExpCond
+from selenium.webdriver.support.wait import WebDriverWait
+
+from .locators import BasePageLocators
 
 
-    def __init__(self, browser, url):
+class BasePage:
+    def __init__(self, browser: Remote, url: str) -> None:
+        super().__init__()
         self.browser = browser
         self.url = url
 
-    def open(self):
+    def open(self) -> None:
         self.browser.get(self.url)
 
-    def __init__(self, browser, url, timeout=10):
-        self.browser = browser
-        self.url = url
-        self.browser.implicitly_wait(timeout)
+    def is_element_present(self, how: str, what: str, timeout=4) -> bool:
 
-    def is_element_present(self, how, what):
         try:
-            self.browser.find_element(how, what)
-        except (NoSuchElementException):
+            WebDriverWait(self.browser, timeout).until(
+                ExpCond.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
+
         return True
+
+    def is_text_present(self, how: str, what: str, text: str) -> bool:
+        return text in self.browser.find_element(how, what).text
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                ExpCond.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1,
+                          TimeoutException).until_not(
+                              ExpCond.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def solve_quiz_and_get_code(self) -> None:
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print(f"Your code: {alert.text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def go_to_login_page(self):
+        self._move_to_page(BasePageLocators.LOGIN_LINK)
+
+    def go_to_cart_page(self):
+        self._move_to_page(BasePageLocators.CART_BUTTON)
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(
+            *BasePageLocators.USER_ICON
+        ), "User icon is not presented, probably unauthorised user"
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            *BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def _move_to_page(self, selector: Sequence[str]) -> None:
+        link = self.browser.find_element(*selector)
+        link.click()
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -8,10 +9,19 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url,  "Wrong URL"
+        assert "login" in self.browser.current_url, "Wrong URL"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login Form is not Presented"
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register Form is not Presented"
+
+    def register_new_user(self, email: str, password: str) -> None:
+        self.browser.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(
+            email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(
+            password)
+        self.browser.find_element(
+            *LoginPageLocators.REPEAT_PASSWORD_INPUT).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
